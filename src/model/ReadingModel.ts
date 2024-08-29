e410225a5b640342b8c9ac9c0f91ee64fc936ba9
Matusalem Gabriel <122733420+matu-gabriel@users.noreply.github.com>
2024-08-29 15:26:49 -0300

    Consertando escrita errada do model

--- src/model/ReadingModel.ts
+++ src/model/ReadingModel.ts
@@ -3,19 +3,17 @@ import { Document, model, Schema } from "mongoose";
 export interface IReading extends Document {
   image: string;
   customer_code: string;
-  measure_datatime: Date;
+  measure_datetime: Date;
   measure_type: "WATER" | "GAS";
-  readingValue: number;
-  createdAt: Date;
+  //   readingValue: number;
 }
 
 const readingSchema = new Schema<IReading>({
   image: { type: String, required: true },
   customer_code: { type: String, required: true },
-  measure_datatime: { type: Date, required: true },
+  measure_datetime: { type: Date, required: true },
   measure_type: { type: String, enum: ["WATER", "GAS"], required: true },
-  readingValue: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
+  //   readingValue: { type: Number, required: true },
 });
 
 const Reading = model<IReading>("Reading", readingSchema);
